451656b3699fb1165185b85cf13464d94138a550
Yash708262 <yash708262@gmail.com>
2025-09-19 16:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function calculateGrade(percentage) {
-  //your code here
+	 if (isNaN(percentage)) {
+        return "F";
+    }
+  let rounded = Math.round(percentage);
+
+    if (rounded >= 90) {
+        return 'A';
+    } else if (rounded >= 80) {
+        return 'B';
+    } else if (rounded >= 70) {
+        return 'C';
+    } else if (rounded >= 60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
